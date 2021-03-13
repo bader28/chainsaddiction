51dae8f48c486459f3051b07d4db7f9a089146f0
Michael Blaß <michael.blass@uni-hamburg.de>
2021-03-13 07:27:37 +0100

    Updated setup

--- setup.py
+++ setup.py
@@ -1,24 +1,16 @@
-#/usr/bin/env python3
-
 from setuptools import setup, Extension
-from numpy.distutils.misc_util import get_numpy_include_dirs
+from setuptools.config import read_configuration
+import numpy as np
 
-setup(
-        name            = 'chainsaddiction',
-        version         = '0.1',
-        description     = 'Discrete time, finit state space, stationary Hidden Markov Model.',
-        install_requires = ['numpy>=1.15.0'],
-        include_dirs    = get_numpy_include_dirs(),
-        ext_modules     =   [
-                                Extension(
-                                    'chainsaddiction',
-                                    sources     = [ 'hmm/stats.c',
-                                                    'hmm/fwbw.c',
-                                                    'hmm/em.c',
-                                                    'hmm/hmm.c',
-                                                    'hmm/hmm_module.c'],
 
-                                include_dirs    = ['./include/'])
-                            ]
-      )
+config = read_configuration('./setup.cfg')
 
+ext_hmm = Extension('chainsaddiction',
+    sources = ['hmm/stats.c',
+               'hmm/fwbw.c',
+               'hmm/em.c',
+               'hmm/hmm.c',
+               'hmm/hmm_module.c'],
+    include_dirs = ['include', np.get_include()])
+
+setup(ext_modules = [ext_hmm])
